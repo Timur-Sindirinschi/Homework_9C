f96b7ff14b5a71b957f6ac6cc5dd9d812acd9efd
Timur Sindirinschi <59025242+Timur-Sindirinschi@users.noreply.github.com>
2019-12-18 18:40:13 +0200

    Add files via upload

---
+++ braD.java
@@ -0,0 +1,24 @@
+
+public class class1234 {
+
+	public static void main(String[] args) {
+	System.out.format("%55s%n","\\ /");
+	System.out.format("%57s%n","-->*<--");
+	System.out.format("%55s%n","/o\\");
+	System.out.format("%56s%n"," /_\\_\\");
+	System.out.format("%57s%n","/_/_0_\\");
+	System.out.format("%58s%n","/_o_\\_\\_\\");
+	System.out.format("%59s%n","/_/_/_/_/o\\");
+	System.out.format("%60s%n","/@\\_\\_\\@\\_\\_\\");
+	System.out.format("%61s%n"," /_/_/O/_/_/_/_\\");
+	System.out.format("%62s%n","/_\\_\\_\\_\\_\\o\\_\\_\\");
+	System.out.format("%63s%n","/_/0/_/_/_0_/_/@/_\\");
+	System.out.format("%64s%n","/_\\_\\_\\_\\_\\_\\_\\_\\_\\_\\");
+	System.out.format("%65s%n","/_/o/_/_/@/_/_/o/_/0/_\\");
+	System.out.format("%56s%n","[___]");
+	
+	
+	
+	}
+
+}
